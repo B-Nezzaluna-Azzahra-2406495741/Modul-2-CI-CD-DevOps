095569bc0c5700f91febaa438a4a075f35dbf819
moondiverc <nezzaluna10@gmail.com>
2026-03-10 13:26:52 +0700

    [REFACTOR] Change PaymentRepository to use Map

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -3,33 +3,24 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class PaymentRepository {
-    private List<Payment> paymentData = new ArrayList<>();
+    private final Map<String, Payment> paymentData = new HashMap<>();
 
     public Payment save(Payment payment) {
-        for (int i = 0; i < paymentData.size(); i++) {
-            if (paymentData.get(i).getId().equals(payment.getId())) {
-                paymentData.set(i, payment);
-                return payment;
-            }
-        }
-        paymentData.add(payment);
+        paymentData.put(payment.getId(), payment);
         return payment;
     }
 
     public Payment findById(String id) {
-        for (Payment payment : paymentData) {
-            if (payment.getId().equals(id)) {
-                return payment;
-            }
-        }
-        return null;
+        return paymentData.get(id);
     }
 
     public List<Payment> findAll() {
-        return new ArrayList<>(paymentData);
+        return new ArrayList<>(paymentData.values());
     }
 }
